6952a07a43a4cb1b17670686ec40c0c8f52f7baf
Ethan Grab <ethangrab@gmail.com>
2019-06-03 12:15:59 -0600

    Seventh commit - serializer test - almost finished

---
+++ spec/serializer/antipode_serializer_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe AntipodeSerializer do
+  context 'instance methods' do
+    it 'outputs correct JSON' do
+      lat_lng = $gmaps.geocode('hongkong')[0][:geometry][:location]
+      antipode_data = AntipodeService.new(lat_lng).antipode
+      search_location = $gmaps.reverse_geocode([22.3193039,114.1693611])
+
+      antipode_forecast_params =
+      {
+        summary: "Clear Skies",
+        temperature: 77.0
+      }
+      binding.pry
+      antipode = Antipode.new(antipode_data, 'hongkong', search_location[0][:formatted_address], antipode_forecast_params)
+
+      #SO CLOSE TO FINISHING THIS TEST!!!
+    end
+  end
+end
