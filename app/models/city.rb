8d6e693d61d069fc1637b2c6b3096e32580348ca
Ethan Grab <ethangrab@gmail.com>
2019-06-05 05:23:08 -0600

    User, city, favorite relationships and favorite model

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,7 +1,10 @@
 class City < ApplicationRecord
+
+  has_many :favorites
+  has_many :users, through: :favorites
+
   has_one :currently_forecast
   has_many :daily_forecasts
   has_many :hourly_forecasts
-
 
 end
