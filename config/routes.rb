9dea2c0fd64b9a1c45f7f927e56c5ac2e8797911
Ethan Grab <ethangrab@gmail.com>
2019-06-05 03:21:25 -0600

    Added route for background get

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       # post '/forecast', to: 'forecasts#create'
       get '/forecast', to: 'forecasts#show'
+      get '/backgrounds', to: 'backgrounds#show'
     end
   end
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
