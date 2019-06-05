1d65925949eeff27d7dd970ebd0ed285d0a9e601
Ethan Grab <ethangrab@gmail.com>
2019-06-05 11:06:44 -0600

    favorite model with relationships

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,21 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :city
+
+
+  def location
+    " #{self.city.city} , #{self.city.state}"
+  end
+
+  def current_weather
+    self.city.currently_forecast
+  end
+
+  def hourly_weather
+    self.city.hourly_forecasts
+  end
+
+  def daily_weather
+    self.city.daily_forecasts
+  end
 end
